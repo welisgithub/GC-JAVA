a66d7a97be838622d74956b00beb373fea3f1a15
welisgithub <welissonfilipe@gmail.com>
2026-06-11 21:38:06 -0300

    12th commit - meio do desafio 1

--- src/DesafioCurso1.java
+++ src/DesafioCurso1.java
@@ -1,5 +1,56 @@
+import java.util.Scanner;
+
+/*
+Inicializar os dados do cliente, menu de opções, consultar saldo, receber e sacar;
+1 - Consultar saldo;
+2 - Saquar dinheiro;
+3 - Depositar dinheiro;
+4 - Encerrar atendimento;
+* */
 public class DesafioCurso1 {
     public static void main(String[] args) {
+        Scanner l = new Scanner(System.in);
+        String nome = "Welisson Andrade";
+        String tipoDeConta = "Corrente";
+        double saldo = 2000.00;
+        int resposta = 0;
 
+        System.out.println("*******************************************");
+        System.out.println("Bem vindo! Segue as informações do cliente: ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo de conta: " + tipoDeConta);
+        System.out.println("*******************************************");
+
+        while (resposta != 4) {
+            System.out.println(" ");
+            System.out.println("*******************************************");
+            System.out.println("O que deseja fazer?");
+            System.out.println("""
+                    1 - Consultar Saldo Atual;
+                    2 - Sacar;
+                    3 - Depositar; 
+                    4 - Encerrar atendimento;
+                    
+                    """);
+            resposta = l.nextInt();
+
+            switch (resposta) {
+                case (1):
+                    System.out.println("Saldo atual de R$" + saldo);
+                    break;
+                case(2):
+                    System.out.println("Quanto você deseja sacar?");
+                    break;
+                case(3):
+                    System.out.println("Quanto você deseja depositar?");
+                    break;
+                case(4):
+                    System.out.println("Atendimento encerrado.");
+                    break;
+                default:
+                    System.out.println("Opção invalida.");
+                    break;
+            }
+        }
     }
 }
